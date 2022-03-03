5022a32b6cf9b9cb182be514d02bb2b900b63fd0
garrettroell <garrettroell@gmail.com>
2022-03-02 21:56:50 -0600

    changed static file line

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const io = new Server(server);
 
 let typingUsers = [];
 
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
