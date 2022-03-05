7d506c13f313c4618f041d498aaebcc992b4cd9e
garrettroell <garrettroell@gmail.com>
2022-03-05 15:05:59 -0600

    Made endpoint that updates when typing user changes

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index2.html");
 });
 
+app.get("/typing", (req, res) => {
+  res.json({ typingUsers: typingUsers });
+});
+
 io.on("connection", (socket) => {
   console.log("a user connected");
   io.emit("typing users", typingUsers);
@@ -36,6 +40,10 @@ io.on("connection", (socket) => {
       typingUsers = [...typingUsers, name];
     }
     io.emit("typing users", typingUsers);
+
+    app.get("/typing", (req, res) => {
+      res.json({ typingUsers: typingUsers });
+    });
   });
 
   socket.on("user not typing", (name) => {
